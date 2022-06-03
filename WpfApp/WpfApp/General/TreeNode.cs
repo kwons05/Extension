e326579e01786d640aafc871177179e5c1201ae1
kwons <102870461+kwons05@users.noreply.github.com>
2022-06-03 18:21:11 +0900

    TreeNode

---
+++ WpfApp/WpfApp/General/TreeNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.General
+{
+    public class TreeNode<T>
+    {
+        List<TreeNode<T>> Children = new List<TreeNode<T>>();
+
+        T Item { get; set; }
+
+        public TreeNode(T item)
+        {
+            Item = item;
+        }
+
+        public TreeNode<T> AddChild(T item)
+        {
+            TreeNode<T> nodeItem = new TreeNode<T>(item);
+            Children.Add(nodeItem);
+            return nodeItem;
+        }
+    }
+
+    public static class Test
+    {
+        public static void Run()
+        {
+            string root = "root";
+            TreeNode<string> myTreeRoot = new TreeNode<string>(root);
+            var first = myTreeRoot.AddChild("first child");
+            var second = myTreeRoot.AddChild("second child");
+            var grandChild = first.AddChild("first child's child");
+        }
+    }
+}
